770760b20b8238044c8f7bccbf32eb4b4bca0450
Gosiuniunia <malgorzata.helt@wp.pl>
2025-04-21 17:04:36 +0200

    hsv and lab extraction

--- face_features_extraction.py
+++ face_features_extraction.py
@@ -3,7 +3,9 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import cv2
 import numpy as np
-from utils import crop_img, GrayworldeWB_algoritm
+import pandas as pd
+from utils import crop_img, apply_kmeans, get_hsv_lab_colour, get_color_between_points
+
 import os
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 
@@ -34,13 +36,26 @@ def extract_iris_colour(img, face_landmarks):
     left_iris_indices  = [469, 145, 471, 159]
     pupil_indices = [468, 473]
     for face_landmarks in face_landmarks:
-        left_iris_img = crop_img(img, face_landmarks, left_iris_indices)
-        right_iris_img = crop_img(img, face_landmarks, right_iris_indices)
+        left_iris_img, left_origin = crop_img(img, face_landmarks, left_iris_indices)
+        right_iris_img, right_origin = crop_img(img, face_landmarks, right_iris_indices)
+        left_iris_centers, segmented_img_li = apply_kmeans(left_iris_img, k=2)
+        right_iris_centers, segmented_img_ri = apply_kmeans(right_iris_img, k=2)
+        left_pupil_colour = get_color_between_points(
+            (face_landmarks[468].x * img.shape[1], face_landmarks[468].y * img.shape[0]), 
+            (face_landmarks[468].x * img.shape[1], face_landmarks[468].y * img.shape[0]), 
+            left_origin, segmented_img_li
+        )
+        right_pupil_colour = get_color_between_points(
+            (face_landmarks[473].x * img.shape[1], face_landmarks[473].y * img.shape[0]), 
+            (face_landmarks[473].x * img.shape[1], face_landmarks[473].y * img.shape[0]), 
+            right_origin, segmented_img_ri
+        )
+        right_iris_colour = right_iris_centers[0] if np.all(right_pupil_colour == right_iris_centers[0]) else right_iris_centers[1]
+        left_iris_colour = left_iris_centers[0] if np.all(left_pupil_colour == left_iris_centers[0]) else left_iris_centers[1]
 
-        cv2.imshow("Left Eye", left_iris_img)
-        cv2.imshow("Right Eye", right_iris_img)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
+        iris_colour = get_hsv_lab_colour([right_iris_colour, left_iris_colour])
+        return iris_colour
+
 
 
 def extract_skin_colour(img, face_landmarks):
@@ -50,36 +65,63 @@ def extract_skin_colour(img, face_landmarks):
     forehead_indices = [103, 67, 109, 10, 338, 297, 332, 104, 69, 108, 151, 337, 299, 333, 9, 8]
 
     all_indices = forehead_indices + cheek_indices + nose_indices + chin_indices
-    for face_landmarks in face_landmarks:
+    skin_colours = []
+    for face in face_landmarks:
         for idx in all_indices:
-            if idx < len(face_landmarks):
-                landmark = face_landmarks[idx]
+            if idx < len(face):
+                landmark = face[idx]
                 x = int(landmark.x * img.shape[1])
                 y = int(landmark.y * img.shape[0])
-                cv2.circle(img, (x, y), 2, (255, 0, 0), -1)
+                if 0 <= x < img.shape[1] and 0 <= y < img.shape[0]:
+                    color = img[y, x]
+                    skin_colours.append(color)
 
-    cv2.imshow('Face Landmarks', img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    skin_colour = get_hsv_lab_colour(skin_colours)
 
+    return skin_colour
 
-def extract_hair_colour(img_rgb, face_landmarks):
+def extract_hair_colour(img, face_landmarks):
     left_eyebrow = [70, 63, 105, 66, 107, 55, 65, 52, 53]
     right_eyebrow = [336, 296, 334, 293, 276, 283, 282, 295, 285]
     for face_landmarks in face_landmarks:
-        left_eyebrow_img = crop_img(img_rgb, face_landmarks, left_eyebrow)
-        right_eyebrow_img = crop_img(img_rgb, face_landmarks, right_eyebrow)
-        cv2.imshow("Left Eyebrow", left_eyebrow_img)
-        cv2.imshow("Right Eyebrow", right_eyebrow_img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        left_eyebrow_img, left_origin = crop_img(img, face_landmarks, left_eyebrow)
+        right_eyebrow_img, right_origin = crop_img(img, face_landmarks, right_eyebrow)
+        left_eyebrow_centers, segmented_img_le = apply_kmeans(left_eyebrow_img, k=2)
+        right_eyebrow_centers, segmented_img_re = apply_kmeans(right_eyebrow_img, k=2)
+        left_eyebrow_colour = get_color_between_points(
+            (face_landmarks[105].x * img.shape[1], face_landmarks[105].y * img.shape[0]), 
+            (face_landmarks[65].x * img.shape[1], face_landmarks[65].y * img.shape[0]), 
+            left_origin, segmented_img_le
+        )
 
-img = cv2.imread(image_path)
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        right_eyebrow_colour = get_color_between_points(
+            (face_landmarks[334].x * img.shape[1], face_landmarks[334].y * img.shape[0]), 
+            (face_landmarks[295].x * img.shape[1], face_landmarks[295].y * img.shape[0]), 
+            right_origin, segmented_img_re
+        )
+    eyebrow_colour = get_hsv_lab_colour([left_eyebrow_colour, right_eyebrow_colour])
+    return eyebrow_colour
 
-FaceLandmarker, options = init_face_landmark(model_path)
-face_landmarks = get_face_landmarks(FaceLandmarker, options, img_rgb)
+def extract_lab_hsv_values_from_photo(image_path):
+    img = cv2.imread(image_path)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-extract_iris_colour(img, face_landmarks)
-extract_skin_colour(img, face_landmarks)
-extract_hair_colour(img, face_landmarks)
+    FaceLandmarker, options = init_face_landmark(model_path)
+    face_landmarks = get_face_landmarks(FaceLandmarker, options, img_rgb)
+
+    iris_colour = extract_iris_colour(img, face_landmarks)
+    skin_colour = extract_skin_colour(img, face_landmarks)
+    eyebrow_colour = extract_hair_colour(img, face_landmarks)
+    return np.concatenate([iris_colour, skin_colour, eyebrow_colour])
+
+
+
+iris_columns = [f"iris_{ch}" for ch in ["L", "a", "b", "H", "S", "V"]]
+skin_columns = [f"skin_{ch}" for ch in ["L", "a", "b", "H", "S", "V"]]
+eyebrow_columns = [f"eyebrow_{ch}" for ch in ["L", "a", "b", "H", "S", "V"]]
+columns = iris_columns + skin_columns + eyebrow_columns
+
+data = extract_lab_hsv_values_from_photo("OIP.jpg")
+df = pd.DataFrame([data], columns=columns)
+
+print(df)
--- utils.py
+++ utils.py
@@ -1,18 +1,53 @@
 import numpy as np
 import cv2
 
+
 def GrayworldeWB_algoritm(img):
     wb = cv2.xphoto.createGrayworldWB()
     wb_img = wb.balanceWhite(img)
     return wb_img
 
+
 def crop_img(img, landmarks, indices):
     h, w, _ = img.shape
-    points = np.array([
-        [int(landmarks[i].x * w), int(landmarks[i].y * h)] for i in indices])
+    points = np.array(
+        [[int(landmarks[i].x * w), int(landmarks[i].y * h)] for i in indices]
+    )
     mask = np.zeros((h, w), dtype=np.uint8)
     cv2.fillPoly(mask, [points], 255)
     masked_img = cv2.bitwise_and(img, img, mask=mask)
     x, y, w_box, h_box = cv2.boundingRect(points)
-    cropped_img = masked_img[y:y+h_box, x:x+w_box]
-    return cropped_img
+    cropped_img = masked_img[y : y + h_box, x : x + w_box]
+    return cropped_img, (x, y)
+
+
+def apply_kmeans(img, k=4):
+    img_data = img.reshape((-1, 3))
+    img_data = np.float32(img_data)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
+    _, labels, centers = cv2.kmeans(
+        img_data, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS
+    )
+    centers = np.uint8(centers)
+    segmented_img = centers[labels.flatten()]
+    segmented_img = segmented_img.reshape(img.shape)
+    return (centers, segmented_img)
+
+
+def get_hsv_lab_colour(bgr_array):
+    avg_bgr = np.mean(bgr_array, axis=0).astype(np.uint8)
+    avg_bgr_reshaped = avg_bgr.reshape((1, 1, 3))
+    avg_lab = cv2.cvtColor(avg_bgr_reshaped, cv2.COLOR_BGR2Lab)[0, 0]
+    avg_hsv = cv2.cvtColor(avg_bgr_reshaped, cv2.COLOR_BGR2HSV)[0, 0]
+    return np.concatenate([avg_lab, avg_hsv])
+
+
+def get_color_between_points(p1, p2, crop_origin, segmented_img):
+    cx = int((p1[0] + p2[0]) / 2) - crop_origin[0]
+    cy = int((p1[1] + p2[1]) / 2) - crop_origin[1]
+
+    h, w = segmented_img.shape[:2]
+    cx = np.clip(cx, 0, w - 1)
+    cy = np.clip(cy, 0, h - 1)
+
+    return segmented_img[cy, cx]
