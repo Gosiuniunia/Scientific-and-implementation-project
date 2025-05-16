dd9563a9f80d472a05fb8bee000a7f7e626af177
weronikadomczewska <w.domczewska@gmail.com>
2025-05-17 00:42:50 +0200

    files source change in dl approach

--- deep_learning_approach.py
+++ deep_learning_approach.py
@@ -8,16 +8,24 @@ import cv2
 import numpy as np
 import os
 from sklearn.metrics import classification_report
-from data_operations import load_images_from_folder_armocromia, load_images_from_folder_showme
+from PIL import image
 
-# dataset1: Korean one
-TEST_IMAGES_PATH_1 = fr"C:\Users\wdomc\Documents\personal_color_analysis\ShowMeTheColor\res\test"
-TRAIN_IMAGES_PATH_1 = rf"C:\Users\wdomc\Documents\personal_color_analysis\ShowMeTheColor\res\train"
+def load_images_from_folder(folder_path, class_map):
+    X = []
+    y = []
+    for color_type_class_name in os.listdir(folder_path):
+        color_type_path = os.path.join(folder_path, color_type_class_name)
+        label = class_map[color_type_class_name]
+        for img_filename in os.listdir(color_type_path):
+            img_path = os.path.join(color_type_path, img_filename)
+            img = cv2.imread(img_path)
+            if img is not None:
+                img = cv2.resize(img, (224, 224))
+                X.append(img)
+                y.append(label)
+    return np.array(X), to_categorical(np.array(y), num_classes=4)
 
-# dataset 2: Italian one
-IMAGES_PATH = rf"C:\Users\wdomc\Documents\personal_color_analysis\raw_face_pictures"
-TRAIN_IMAGES_PATH_2 = rf"{IMAGES_PATH}\train"
-TEST_IMAGES_PATH_2 = rf"{IMAGES_PATH}\test"
+IMAGES_PATH = rf"C:\Users\wdomc\Documents\personal_color_analysis\dataset_PColA"
 
 # Parameters
 input_shape = (224, 224, 3)
@@ -36,10 +44,9 @@ model.compile(optimizer=Adam(learning_rate=0.001),
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-class_map_1 = {'fall': 0, 'spring': 1, 'summer': 2, 'winter': 3}
-class_map_2 = {'primavera': 0, 'estate': 1, 'autunno': 2, 'inverno': 3}
+class_map = {'fall': 0, 'spring': 1, 'summer': 2, 'winter': 3}
 
-# X, y = load_images_from_folder_showme(TRAIN_IMAGES_PATH_1, class_map_1)
+# X, y = load_images_from_folder_showme(IMAGES_PATH_1, class_map)
 # history = model.fit(
 #     X, y,
 #     epochs=30,
@@ -50,7 +57,7 @@ class_map_2 = {'primavera': 0, 'estate': 1, 'autunno': 2, 'inverno': 3}
 # model.save(rf'C:\Users\wdomc\Documents\personal_color_analysis\model_weights\basic_vgg16.keras')
 
 test_model = keras.models.load_model(rf"C:\Users\wdomc\Documents\personal_color_analysis\model_weights\basic_vgg16.keras")
-X_test, y_test = load_images_from_folder_armocromia(TEST_IMAGES_PATH_2, class_map_2)
+X_test, y_test = load_images_from_folder(IMAGES_PATH, class_map)
 loss, accuracy = test_model.evaluate(X_test, y_test, verbose=1)
 
 print(f"Test Accuracy: {accuracy:.2f}")
@@ -59,4 +66,4 @@ print(f"Test Loss: {loss:.2f}")
 y_pred_probs = test_model.predict(X_test)
 y_pred = np.argmax(y_pred_probs, axis=1)
 y_true = np.argmax(y_test, axis=1)
-print(classification_report(y_true, y_pred, target_names=class_map_2.keys()))
+print(classification_report(y_true, y_pred, target_names=class_map.keys()))
