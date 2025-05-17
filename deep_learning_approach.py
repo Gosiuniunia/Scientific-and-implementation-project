260d4181ec2b7366d4360125648d638e877bf73d
weronikadomczewska <w.domczewska@gmail.com>
2025-05-17 16:06:56 +0200

    added kfolds to dl approach and trained 5 models

--- deep_learning_approach.py
+++ deep_learning_approach.py
@@ -1,68 +1,118 @@
-from tensorflow import keras
-from tensorflow.keras.utils import to_categorical
+import pandas as pd
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications.vgg16 import preprocess_input
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.applications import VGG16
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense
-import cv2
+from tensorflow.keras.metrics import Precision, Recall
 import numpy as np
-import os
 from sklearn.metrics import classification_report
 
-def load_images_from_folder(folder_path, class_map):
-    X = []
-    y = []
-    for color_type_class_name in os.listdir(folder_path):
-        color_type_path = os.path.join(folder_path, color_type_class_name)
-        label = class_map[color_type_class_name]
-        for img_filename in os.listdir(color_type_path):
-            img_path = os.path.join(color_type_path, img_filename)
-            img = cv2.imread(img_path)
-            if img is not None:
-                img = cv2.resize(img, (224, 224))
-                X.append(img)
-                y.append(label)
-    return np.array(X), to_categorical(np.array(y), num_classes=4)
+"""
+
+This script:
+
+- performs a transfer learning of CNN VGG16 model, using provided image data, so it can perform seasonal type classification
+- collects model training, evaluation and classification statistics like 
+
+- Kfolds validation is used in the learning process
+
+"""
 
 IMAGES_PATH = rf"C:\Users\wdomc\Documents\personal_color_analysis\dataset_PColA"
+MODEL_FREE_AUGMENTED_IMAGES_PATH = rf"C:\Users\wdomc\Documents\personal_color_analysis\dataset_PCoIA_model_free_augmented"
+MODEL_BASED_AUGMENTED_IMAGES_PATH = rf"C:\Users\wdomc\Documents\personal_color_analysis\dataset_PCoIA_model_based_augmented"
+FOLDS_ASSIGNMENT_PATH = rf"data/fold_assignments.csv"
 
 # Parameters
+batch_size = 32
+target_size = (224, 224)
 input_shape = (224, 224, 3)
 num_classes = 4
+k = 5
 
-base_model = VGG16(include_top=True, weights='imagenet', input_shape=input_shape)
-x = base_model.layers[-2].output
-output = Dense(num_classes, activation='softmax')(x)
+datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
 
-model = Model(inputs=base_model.input, outputs=output)
-for layer in base_model.layers:
-            layer.trainable = False
+# Retrieving and adjusting folds assignments details
+df = pd.read_csv(FOLDS_ASSIGNMENT_PATH)
+df['filename'] = df['label'] + '/' + df['filename']
 
-model.summary()
-model.compile(optimizer=Adam(learning_rate=0.001),
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+for fold in range(k):
+    print(f"Training fold {fold}...")
 
-class_map = {'fall': 0, 'spring': 1, 'summer': 2, 'winter': 3}
+    # Splitting data into test and train according to provided assignment
+    train_df = df[df['kfold'] != fold]
+    test_df = df[df['kfold'] == fold]
 
-# X, y = load_images_from_folder_showme(IMAGES_PATH_1, class_map)
-# history = model.fit(
-#     X, y,
-#     epochs=30,
-#     # callbacks=[callbacks_list],
-#     verbose=True,
-#     shuffle=True)
-#
-# model.save(rf'C:\Users\wdomc\Documents\personal_color_analysis\model_weights\basic_vgg16.keras')
+    datagen = ImageDataGenerator(rescale=1./255)
 
-test_model = keras.models.load_model(rf"C:\Users\wdomc\Documents\personal_color_analysis\model_weights\basic_vgg16.keras")
-X_test, y_test = load_images_from_folder(IMAGES_PATH, class_map)
-loss, accuracy = test_model.evaluate(X_test, y_test, verbose=1)
+    train_gen = datagen.flow_from_dataframe(
+        dataframe=train_df,
+        directory=IMAGES_PATH,
+        x_col='filename',
+        y_col='label',
+        target_size=target_size,
+        class_mode='categorical',
+        batch_size=batch_size,
+        shuffle=True
+    )
 
-print(f"Test Accuracy: {accuracy:.2f}")
-print(f"Test Loss: {loss:.2f}")
+    test_gen = datagen.flow_from_dataframe(
+        dataframe=test_df,
+        directory=IMAGES_PATH,
+        x_col='filename',
+        y_col='label',
+        target_size=target_size,
+        class_mode='categorical',
+        batch_size=batch_size,
+        shuffle=False
+    )
 
-y_pred_probs = test_model.predict(X_test)
-y_pred = np.argmax(y_pred_probs, axis=1)
-y_true = np.argmax(y_test, axis=1)
-print(classification_report(y_true, y_pred, target_names=class_map.keys()))
+    # model definition
+    base_model = VGG16(include_top=True, weights='imagenet', input_shape=input_shape)
+    x = base_model.layers[-2].output
+    output = Dense(num_classes, activation='softmax')(x)
+
+    model = Model(inputs=base_model.input, outputs=output)
+    for layer in base_model.layers:
+                layer.trainable = False
+
+    # model.summary()
+    model.compile(optimizer=Adam(learning_rate=0.001),
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy', Precision(name='precision'), Recall(name='recall')])
+
+    # model training
+    history = model.fit(train_gen, epochs=5, verbose=True)
+    model.save(rf'C:\Users\wdomc\Documents\personal_color_analysis\model_weights\basic_vgg16_fold_{fold}.keras')
+
+    # training statistics
+    np.save(f'scores/fold{fold}_training_history.npy', history.history, allow_pickle=True)
+    precision = np.array(history.history['precision'])
+    recall = np.array(history.history['recall'])
+    f1 = 2 * (precision * recall) / (precision + recall + 1e-7)
+    np.save(f'scores/fold{fold}_f1.npy', f1)
+
+    # Model evaluation
+    loss, accuracy, precision, recall = model.evaluate(test_gen, verbose=1)
+    print(f"Loss: {loss:.4f}, Acc: {accuracy:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}")
+
+    eval_metrics = np.array([loss, accuracy, precision, recall])
+    np.save(f'scores/fold{fold}_test_metrics.npy', eval_metrics)
+
+    # Using model for prediction on test data
+    y_pred_probs = model.predict(test_gen)
+    y_pred = np.argmax(y_pred_probs, axis=1)
+    y_true = test_gen.classes
+
+    # Saving classification results
+    np.save(f'scores/fold{fold}_y_pred.npy', y_pred)
+    np.save(f'scores/fold{fold}_y_true.npy', y_true)
+
+    # Saving classification statistics
+    report_dict = classification_report(y_true, y_pred, target_names=['fall', 'spring', 'summer', 'winter'],
+                                        output_dict=True)
+    print(report_dict)
+
+    np.save(f'scores/fold{fold}_prediction_report.npy', report_dict, allow_pickle=True)
